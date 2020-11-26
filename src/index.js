8cea184f954edf3efb624ee011b8e9a52fc3204f
Scott M. Wyant <29161635+scottmwyant@users.noreply.github.com>
2020-11-25 21:33:17 -0500

    Provide outputs, tag and version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const matchVersion = require("./match-version");
+const core = require("@actions/core");
 
 try {
-  matchVersion(process.env.GITHUB_REF, process.env.TAG_PREFIX);
+  const {tag, version} = matchVersion(process.env.GITHUB_REF, process.env.TAG_PREFIX);
   console.info(`Git tag (${tag}) matches package.json version (${version})`);
+  core.setOutput("tag", tag);
+  core.setOutput("version", version);
 } catch (error) {
   console.error(error.message);
   process.exit(1);
