de19ecc3bc9f9abf71556f2aa6d9916d2c4181b1
Scott M. Wyant <29161635+scottmwyant@users.noreply.github.com>
2020-11-26 23:12:26 -0500

    Testing an input as env variable

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const matchVersion = require("./match-version");
 const core = require("@actions/core");
 
 try {
-  const {tag, version} = matchVersion(process.env.GITHUB_REF, process.env.TAG_PREFIX);
+  const prefix = process.env.INPUT_TAG_PREFIX ? process.env.INPUT_TAG_PREFIX : process.env.TAG_PREFIX;
+  const {tag, version} = matchVersion(process.env.GITHUB_REF, prefix);
   console.info(`Git tag (${tag}) matches package.json version (${version})`);
   core.setOutput("tag", tag);
   core.setOutput("version", version);
